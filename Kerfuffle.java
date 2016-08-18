d5ad3d3eeb08280bc7a3cd28a79a5ed76246b0eb
MilindPathiyal <milindpathiyal@gmail.com>
2016-08-17 20:43:33 -0700

    Add files via upload

---
+++ Kerfuffle.java
@@ -0,0 +1,30 @@
+//Miind Pathiyal
+
+//Kerfuffle
+
+public class Kerfuffle
+{
+    public Kerfuffle()
+    {
+    }
+    public Kerfuffle(Kerfuffle x)
+    {
+    }
+    public static double doff(Kerfuffle x, Kerfuffle y, String s)
+    {
+        return 0.0;
+    }
+    public void cachinnate(String s)
+    {
+    }
+    public int vituperate(double x)
+    {
+        return 1;
+    }
+    public String bilk(int x)
+    {
+        return "hi";
+    }
+    
+    
+}
---
+++ KerfuffleDriver.java
@@ -0,0 +1,17 @@
+//Miind Pathiyal
+
+//Kerfuffle
+
+
+public class KerfuffleDriver
+{
+    public static void start()
+    {
+        Kerfuffle knave = new Kerfuffle();
+        int abecedarian = knave.vituperate(4.2);
+        String quagmire = knave.bilk(abecedarian);
+        knave.cachinnate(quagmire);
+        Kerfuffle lalochezia = new Kerfuffle(knave);
+        double philosphunculist = Kerfuffle.doff(knave, lalochezia, quagmire);
+    }     
+}
